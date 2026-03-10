04f94f56bd17ede5cbc442d0d8245d8a6617bce4
lojaalves4 <lojaalves4@gmail.com>
2026-03-10 18:41:27 -0300

    Update calculos_lib.py

--- calculos_lib.py
+++ calculos_lib.py
@@ -1,7 +1,6 @@
-def format_orc(orc_num):
-    return f"ORC-{orc_num:04d}"
-
 def calcular_preco(custo_material, horas, valor_hora, despesas, margem):
+    # Calcula o custo total
     custo_total = custo_material + (horas * valor_hora) + despesas
+    # Aplica a margem de lucro
     preco_final = custo_total * (1 + (margem / 100))
     return custo_total, preco_final
